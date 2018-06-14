d7f94e408facd2c2a0185dc6391c8ecb91f57b87
Charles Watt <cwatt@princeton.edu>
2018-06-14 11:05:20 -0400

    updated the program to be able to use the new filing system

--- JDXConverterUserInterface.py
+++ JDXConverterUserInterface.py
@@ -13,95 +13,95 @@ from numpy import genfromtxt
 
 
 def createArray(jcampDict, filename):
-	DataArray =[]
+    DataArray =[]
 
-	counterY=0
-	counter =0
-	for number in jcampDict['x']:	
-		counter = counter +1
-		counterY2=0
-		while counter != float(number):
-		
-			#print float(number)
-			DataArray.append(0)
-			#fileData.write('%f,'%zero)
-			#fileData.write('\n')
-			counter = counter +1 
+    counterY=0
+    counter =0
+    for number in jcampDict['x']:    
+        counter = counter +1
+        counterY2=0
+        while counter != float(number):
+        
+            #print float(number)
+            DataArray.append(0)
+            #fileData.write('%f,'%zero)
+            #fileData.write('\n')
+            counter = counter +1 
 
-		for number2 in jcampDict['y']:
-			if counterY2 == counterY:
-				
-				DataArray.append(number2) 
-				#fileData.write('%f,'%number2)
-				#fileData.write('\n')
-				#counterY2 =counterY2 + 1
-				break;
-			else:
-				counterY2=counterY2 +1  
-
-
-		counterY= counterY +1
+        for number2 in jcampDict['y']:
+            if counterY2 == counterY:
+                
+                DataArray.append(number2) 
+                #fileData.write('%f,'%number2)
+                #fileData.write('\n')
+                #counterY2 =counterY2 + 1
+                break;
+            else:
+                counterY2=counterY2 +1  
 
 
-	if len(DataArray) < 100:
-		for i in range(len(DataArray), 100):
-			DataArray.append(0)
-			#print i
+        counterY= counterY +1
 
-	return DataArray
+
+    if len(DataArray) < 100:
+        for i in range(len(DataArray), 100):
+            DataArray.append(0)
+            #print i
+
+    return DataArray
 
 def combineArray(Array1, Array2):
-	
-	for i in range(100):
-		Array1.append(Array2[i])
-	
-	
-	return Array1
+    
+    for i in range(100):
+        Array1.append(Array2[i])
+    
+    
+    return Array1
 
 def exportToCSV(filename, OverallArray, listOfFiles, MoleculeNames, ENumbers, MWeights):
 
-	f5 = open(filename, 'w')
+    f5 = open(filename, 'w')
 
-	#write the molecues
-	f5.write('Information/Notes: ')
-	f5.write('\n')
-	f5.write('Molecues,')
-	for i in MoleculeNames:
-		f5.write('%s,' %i)
-		#print i
-	f5.write('\n')
+    #write the molecues
+    f5.write('Information/Notes: ')
+    f5.write('\n')
+    f5.write('Molecues,')
+    for i in MoleculeNames:
+        f5.write('%s,' %i)
+        #print i
+    f5.write('\n')
 
-	#write the Electron Numbers
-	f5.write('Electron number,')
-	for i in ENumbers:
-		f5.write('%f,'%(int(i)))
-		#print i
-	f5.write('\n')
+    #write the Electron Numbers
+    f5.write('Electron number,')
+    for i in ENumbers:
+        f5.write('%f,'%(int(i)))
+        #print i
+    f5.write('\n')
 
-	f5.write('Molecular Weight,')
-	for i in MWeights:
-		f5.write('%f,' %(float(i)))
-		#print i
-	f5.write('\n')
+    f5.write('Molecular Weight,')
+    for i in MWeights:
+        f5.write('%f,' %(float(i)))
+        #print i
+    f5.write('\n')
 
-	Array1=OverallArray
-	printRow= len(Array1)//100
-	printArray =[]
-	zeros = True
+    Array1=OverallArray
+    printRow= len(Array1)//100
+    printArray =[]
+    zeros = True
 
 
-	
-	for i in range(100):
-		zeros = True
-		for k in range(printRow):
-			if Array1[100*k +i] != 0:
-				zeros =False				
-		if zeros == False:
-			f5.write('%d,'%(i))	
-			for y in range(printRow):	
-				f5.write('%d,'%(Array1[100*y +i-1]))
-			f5.write('\n')
-		
+    
+    for i in range(100):
+        zeros = True
+        for k in range(printRow):
+            if Array1[100*k +i] != 0:
+                zeros =False                
+        if zeros == False:
+            f5.write('%d,'%(i))    
+            for y in range(printRow):    
+                f5.write('%d,'%(Array1[100*y +i-1]))
+            f5.write('\n')
+        
 
 moleculeName=''
 MoleculeNames=list()
@@ -120,52 +120,56 @@ fileYorN=input()
 
 
 if (fileYorN =='no'):
-	print("Welcome! Enter the name of the molecule, it's mass, it's electron number and the associated JDX file in order to generate your raw data fields")
-	print("Enter the molecule's Name: ")
-	moleculeName = input()
+    print("Welcome! Enter the name of the molecule, it's mass, it's electron number and the associated JDX file in order to generate your raw data fields")
+    print("Enter the molecule's Name: ")
+    moleculeName = input()
 
 
 
-	while moleculeName != 'EXIT':
-		MoleculeNames.append(moleculeName)
-		print(" enter the electron Number: ")
-		ENumber = input()
-		ENumbers.append(ENumber)
-		print(" enter the Molecular Weight:")
-		MWeight= input()
-		MWeights.append(MWeight)
-		print("enter the file name(EX: oxygenMass.jdx): ")
-		filename=input()
-		listOfFiles.append(filename)
-		print("Enter the name of the next molecule or type EXIT to finish entering molecules")
-		moleculeName=input()
+    while moleculeName != 'EXIT':
+        MoleculeNames.append(moleculeName)
+        print(" enter the electron Number: ")
+        ENumber = input()
+        ENumbers.append(ENumber)
+        print(" enter the Molecular Weight:")
+        MWeight= input()
+        MWeights.append(MWeight)
+        print("enter the file name(EX: oxygenMass.jdx): ")
+        filename=input()
+        listOfFiles.append(filename)
+        print("Enter the name of the next molecule or type EXIT to finish entering molecules")
+        moleculeName=input()
 
 elif(fileYorN=='yes'):
-	fileInputName=''
-	print("enter the file input name please:")
-	fileInputName=input()
-	#input_file ='attempt.csv'
-	list_holder=[]
-	spamReader = csv.reader(open('%s' %fileInputName), delimiter=',')
-	for row in spamReader:
-		list_holder.append(row)
+    fileInputName=''
+    print("enter the file input name please:")
+    fileInputName=input()
+    #input_file ='attempt.csv'
+    list_holder=[]
+    spamReader = csv.reader(open('%s' %fileInputName), delimiter=',')
+    for row in spamReader:
+        list_holder.append(row)
 
 
-	for i in range(1, len(list_holder)):
+    for i in range(1, len(list_holder)):
+        
+        "The below line has been added to allow the program to draw files from \
+        outside it's own directory." 
+        list_holder[i][3] = "Documentation\\" + list_holder[i][3]
 
-		MoleculeNames.append(list_holder[i][0])
-		ENumbers.append(list_holder[i][1])
-		MWeights.append(list_holder[i][2])
-		listOfFiles.append(list_holder[i][3])
-		
+        MoleculeNames.append(list_holder[i][0])
+        ENumbers.append(list_holder[i][1])
+        MWeights.append(list_holder[i][2])
+        listOfFiles.append(list_holder[i][3])
+        
 
 
 
 OverallArray=[]
 holderArray=[]
 for i in listOfFiles:
-	jcampDict=JDXConverter.JCAMP_reader(i)
-	holderArray=createArray(jcampDict, i)
-	OverallArray=combineArray(OverallArray, holderArray)
+    jcampDict=JDXConverter.JCAMP_reader(i)
+    holderArray=createArray(jcampDict, i)
+    OverallArray=combineArray(OverallArray, holderArray)
 
 exportToCSV("ConvertedSpectra.csv", OverallArray, listOfFiles, MoleculeNames, ENumbers, MWeights)
