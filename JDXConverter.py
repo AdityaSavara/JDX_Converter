785fb11b9d6c6831f4e209f5734708e52d2282c2
XLR8-07 <joyashikur@gmail.com>
2021-10-16 20:58:04 +0600

    "exportCSV single JDX file call is showing an error"

--- JDXConverter.py
+++ JDXConverter.py
@@ -34,7 +34,7 @@ def createArray(jcampDict, filename):
                 #fileData.write('%f,'%number2)
                 #fileData.write('\n')
                 #counterY2 =counterY2 + 1
-                break;
+                break
             else:
                 counterY2=counterY2 +1  
 
@@ -53,7 +53,7 @@ def combineArray(Array1, Array2):
     
     return Array1
 
-def exportToCSV(filename, OverallArray, listOfFiles, MoleculeNames, ENumbers, MWeights, knownMoleculeIonizationTypes, knownIonizationFactorsRelativeToN2, SourceOfFragmentationPatterns, SourceOfIonizationData):
+def exportToCSV(filename, OverallArray, MoleculeNames, ENumbers, MWeights, knownMoleculeIonizationTypes, knownIonizationFactorsRelativeToN2, SourceOfFragmentationPatterns, SourceOfIonizationData):
 
     
     f5 = open(filename, 'w')
@@ -125,128 +125,128 @@ def exportToCSV(filename, OverallArray, listOfFiles, MoleculeNames, ENumbers, MW
             
     f5.close()
       
-SourceOfFragmentationPattern = ''
-SourceOfFragmentationPatterns = list()
-SourceOfIonizationDatum = ''
-SourceOfIonizationData = list()
-moleculeName=''
-MoleculeNames=list()
-ENumber = 0
-ENumbers =list()
-MWeight =0.0 
-MWeights=list()
-knownMoleculeIonizationType = ''
-knownMoleculeIonizationTypes = list()
-knownIonizationFactorRelativeToN2 = 0.0
-knownIonizationFactorsRelativeToN2 = list()
-filenames=''
-listOfFiles=list()
+# SourceOfFragmentationPattern = ''
+# SourceOfFragmentationPatterns = list()
+# SourceOfIonizationDatum = ''
+# SourceOfIonizationData = list()
+# moleculeName=''
+# MoleculeNames=list()
+# ENumber = 0
+# ENumbers =list()
+# MWeight =0.0 
+# MWeights=list()
+# knownMoleculeIonizationType = ''
+# knownMoleculeIonizationTypes = list()
+# knownIonizationFactorRelativeToN2 = 0.0
+# knownIonizationFactorsRelativeToN2 = list()
+# filenames=''
+# listOfFiles=list()
 
 
-fileYorN=''
+# fileYorN=''
 
-print("would you like to load molecular information from a csv file? Enter 'yes' or 'no'. If not, then you will enter files manually.")
-fileYorN=input()
-
-
-if (fileYorN =='no'):
-    print("Welcome! Enter the name of the molecule, its mass, its ionization factor relative to nitrogen (put unknown if you don't know), its ionization type (put unknown if you don't know), its number of electrons (or the number -1 if you don't need that), and the associated JDX file in order to generate a csv spectrum file")
-    print("If a molecule name has a comma in it (e.g. 1,3-pentadiene) or any other input has a comma in it, we recommend using an _ (e.g. 1_3-pentadiene) since this information is stored in a comma separated value file.")
-    print("Enter the molecule's Name: ")
-    moleculeName = input()
+# print("would you like to load molecular information from a csv file? Enter 'yes' or 'no'. If not, then you will enter files manually.")
+# fileYorN=input()
 
 
+# if (fileYorN =='no'):
+#     print("Welcome! Enter the name of the molecule, its mass, its ionization factor relative to nitrogen (put unknown if you don't know), its ionization type (put unknown if you don't know), its number of electrons (or the number -1 if you don't need that), and the associated JDX file in order to generate a csv spectrum file")
+#     print("If a molecule name has a comma in it (e.g. 1,3-pentadiene) or any other input has a comma in it, we recommend using an _ (e.g. 1_3-pentadiene) since this information is stored in a comma separated value file.")
+#     print("Enter the molecule's Name: ")
+#     moleculeName = input()
 
-    while moleculeName != 'EXIT':
-        MoleculeNames.append(moleculeName)
-        print(" enter the electron Number: ")
-        ENumber = input()
-        ENumbers.append(ENumber)
-        print(" enter the molecule's ionization type (Enter unknown if unknown): ")
-        knownMoleculeIonizationType = input()
-        knownMoleculeIonizationTypes.append(knownMoleculeIonizationType)
-        print(" enter the molecule's ionization factor relative to N2 (Enter a unknown if unknown): ")
-        knownIonizationFactorRelativeToN2 = input()
-        knownIonizationFactorsRelativeToN2.append(knownIonizationFactorRelativeToN2)
-        print(" enter the source of the fragmention pattern: ")
-        SourceOfFragmentationPattern = input()
-        SourceOfFragmentationPatterns.append(SourceOfFragmentationPattern)
-        print(" enter the source of the ionization data: ")
-        SourceOfIonizationDatum = input()
-        SourceOfIonizationData.append(SourceOfIonizationDatum)
-        print(" enter the Molecular Weight:")
-        MWeight= input()
-        MWeights.append(MWeight)        
-        print("enter the file name(EX: oxygen.jdx):")
-        print("If the file is in a separate directory, \ninclude the path(EX: JDXFiles\oxygen.jdx):")
-        filename=input()
-        listOfFiles.append(filename)
-        print("Enter the name of the next molecule or type EXIT to finish entering molecules")
-        moleculeName=input()
 
-elif(fileYorN=='yes'):
-    fileInputName=''
-    print("enter the file input name please:")
-    fileInputName=input()
-    #input_file ='attempt.csv'
-    list_holder=[]
-    spamReader = csv.reader(open('%s' %fileInputName), delimiter=',')
-    for row in spamReader:
-        list_holder.append(row)
+
+#     while moleculeName != 'EXIT':
+#         MoleculeNames.append(moleculeName)
+#         print(" enter the electron Number: ")
+#         ENumber = input()
+#         ENumbers.append(ENumber)
+#         print(" enter the molecule's ionization type (Enter unknown if unknown): ")
+#         knownMoleculeIonizationType = input()
+#         knownMoleculeIonizationTypes.append(knownMoleculeIonizationType)
+#         print(" enter the molecule's ionization factor relative to N2 (Enter a unknown if unknown): ")
+#         knownIonizationFactorRelativeToN2 = input()
+#         knownIonizationFactorsRelativeToN2.append(knownIonizationFactorRelativeToN2)
+#         print(" enter the source of the fragmention pattern: ")
+#         SourceOfFragmentationPattern = input()
+#         SourceOfFragmentationPatterns.append(SourceOfFragmentationPattern)
+#         print(" enter the source of the ionization data: ")
+#         SourceOfIonizationDatum = input()
+#         SourceOfIonizationData.append(SourceOfIonizationDatum)
+#         print(" enter the Molecular Weight:")
+#         MWeight= input()
+#         MWeights.append(MWeight)        
+#         print("enter the file name(EX: oxygen.jdx):")
+#         print("If the file is in a separate directory, \ninclude the path(EX: JDXFiles\oxygen.jdx):")
+#         filename=input()
+#         listOfFiles.append(filename)
+#         print("Enter the name of the next molecule or type EXIT to finish entering molecules")
+#         moleculeName=input()
+
+# elif(fileYorN=='yes'):
+#     fileInputName=''
+#     print("enter the file input name please:")
+#     fileInputName=input()
+#     #input_file ='attempt.csv'
+#     list_holder=[]
+#     spamReader = csv.reader(open('%s' %fileInputName), delimiter=',')
+#     for row in spamReader:
+#         list_holder.append(row)
         
         
-#The user is provided with the option to direct the functions output as they would like.  
-print("Would you like to specify an output location? If yes, type the path to the location. For default, hit enter.")
-outputDirectory = input()
-#If the user selects default, then the output is piped to "OutputFiles"
-if outputDirectory == "":
-    outputDirectory = "OutputFiles"
+# #The user is provided with the option to direct the functions output as they would like.  
+# print("Would you like to specify an output location? If yes, type the path to the location. For default, hit enter.")
+# outputDirectory = input()
+# #If the user selects default, then the output is piped to "OutputFiles"
+# if outputDirectory == "":
+#     outputDirectory = "OutputFiles"
 
 
-#mkaing the directory for exported files, if it isn't already there
-if not os.path.exists(outputDirectory):
-  os.makedirs(outputDirectory)
+# #mkaing the directory for exported files, if it isn't already there
+# if not os.path.exists(outputDirectory):
+#   os.makedirs(outputDirectory)
 
-#only if files exist to draw from
-if fileYorN == 'yes':
-#Checking if a directory exists to be drawn from
-    if os.path.isdir("JDXFiles"):
-    #This for loop draws from a directory of the user's choice (hard-coded)
-        for i in range(1, len(list_holder)):
+# #only if files exist to draw from
+# if fileYorN == 'yes':
+# #Checking if a directory exists to be drawn from
+#     if os.path.isdir("JDXFiles"):
+#     #This for loop draws from a directory of the user's choice (hard-coded)
+#         for i in range(1, len(list_holder)):
             
-# The below line has been added to allow the program to draw files from 
-  # outside it's own directory.
-            list_holder[i][3] = "JDXFiles\\" + list_holder[i][3]
+# # The below line has been added to allow the program to draw files from 
+#   # outside it's own directory.
+#             list_holder[i][3] = "JDXFiles\\" + list_holder[i][3]
     
-            MoleculeNames.append(list_holder[i][0])
-            ENumbers.append(list_holder[i][1])
-            MWeights.append(list_holder[i][2])
-            listOfFiles.append(list_holder[i][3])
-            knownMoleculeIonizationTypes.append(list_holder[i][4])
-            knownIonizationFactorsRelativeToN2.append(list_holder[i][5])
-            SourceOfFragmentationPatterns.append(list_holder[i][6])
-            SourceOfIonizationData.append(list_holder[i][7])
+#             MoleculeNames.append(list_holder[i][0])
+#             ENumbers.append(list_holder[i][1])
+#             MWeights.append(list_holder[i][2])
+#             listOfFiles.append(list_holder[i][3])
+#             knownMoleculeIonizationTypes.append(list_holder[i][4])
+#             knownIonizationFactorsRelativeToN2.append(list_holder[i][5])
+#             SourceOfFragmentationPatterns.append(list_holder[i][6])
+#             SourceOfIonizationData.append(list_holder[i][7])
             
-#Otherwise, assume that the files are in the directory of the JDXConv-UI
-    else:
+# #Otherwise, assume that the files are in the directory of the JDXConv-UI
+#     else:
     
-#This for loop draws files from the current directory
-        for i in range(1, len(list_holder)):
+# #This for loop draws files from the current directory
+#         for i in range(1, len(list_holder)):
             
-            MoleculeNames.append(list_holder[i][0])
-            ENumbers.append(list_holder[i][1])
-            MWeights.append(list_holder[i][2])
-            listOfFiles.append(list_holder[i][3])
-            knownMoleculeIonizationTypes.append(list_holder[i][4])
-            knownIonizationFactorsRelativeToN2.append(list_holder[i][5])
-            SourceOfFragmentationPatterns.append(list_holder[i][6])
-            SourceOfIonizationData.append(list_holder[i][7])
+#             MoleculeNames.append(list_holder[i][0])
+#             ENumbers.append(list_holder[i][1])
+#             MWeights.append(list_holder[i][2])
+#             listOfFiles.append(list_holder[i][3])
+#             knownMoleculeIonizationTypes.append(list_holder[i][4])
+#             knownIonizationFactorsRelativeToN2.append(list_holder[i][5])
+#             SourceOfFragmentationPatterns.append(list_holder[i][6])
+#             SourceOfIonizationData.append(list_holder[i][7])
 
-OverallArray=[]
-holderArray=[]
-for i in listOfFiles:
-    jcampDict=JCampSG.JCAMP_reader(i)
-    holderArray=createArray(jcampDict, i)
-    OverallArray=combineArray(OverallArray, holderArray)
+# OverallArray=[]
+# holderArray=[]
+# for i in listOfFiles:
+#     jcampDict=JCampSG.JCAMP_reader(i)
+#     holderArray=createArray(jcampDict, i)
+#     OverallArray=combineArray(OverallArray, holderArray)
 
-exportToCSV("%s\\ConvertedSpectra.csv" %outputDirectory, OverallArray, listOfFiles, MoleculeNames, ENumbers, MWeights, knownMoleculeIonizationTypes, knownIonizationFactorsRelativeToN2, SourceOfFragmentationPatterns, SourceOfIonizationData)
+exportToCSV("%s\\ConvertedSpectra.csv" %outputDirectory, OverallArray,  MoleculeNames, ENumbers, MWeights, knownMoleculeIonizationTypes, knownIonizationFactorsRelativeToN2, SourceOfFragmentationPatterns, SourceOfIonizationData)
