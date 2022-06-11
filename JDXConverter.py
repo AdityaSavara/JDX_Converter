559f8a20605cf0daefd290fa193f9792491bdea7
XLR8-07 <joyashikur@gmail.com>
2022-06-12 03:05:48 +0600

    Replaced the prompt string outside the loop for first time entry prompt

--- JDXConverter.py
+++ JDXConverter.py
@@ -599,9 +599,9 @@ def newStartCommandLine(dataBaseFileName='MoleculesInfo.csv', defaultJDXFilesLoc
 
     print("WELCOME!")
     print("If a molecule name has a comma in it (e.g. 1,3-pentadiene) or any other input has a comma in it, we recommend using an _ (e.g. 1_3-pentadiene) since this information is stored in a comma separated value file.")
-
+    print("ENTER A MOLECULE NAME, OR MULTIPLE MOLECULE NAMES. Separate multiple names using ';'.")
     while True:
-        print("ENTER A MOLECULE NAME, OR MULTIPLE MOLECULE NAMES. Separate multiple names using ';'.")
+        
         moleculeName = input()
         if(moleculeName == 'END'): break
         if(';' in moleculeName):
